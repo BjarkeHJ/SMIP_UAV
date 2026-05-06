6959d251deb66c28b541d391b16a35552bcb5dde
BjarkeHJ <bjarke_holm@hotmail.com>
2026-05-06 13:09:00 +0200

    working odometry both sim and onboard

--- smip_uav/src/smip_exec.cpp
+++ smip_uav/src/smip_exec.cpp
@@ -26,16 +26,12 @@ int main(int argc, char** argv) {
 
     rclcpp::executors::MultiThreadedExecutor smip_exec;
     smip_exec.add_node(map_node);
-
+    
     std::shared_ptr<VoxlTfNode> ov_tf_node;
-    ov_tf_node = std::make_shared<VoxlTfNode>();
-    smip_exec.add_node(ov_tf_node);
-
-    // std::shared_ptr<VoxlTfNode> ov_tf_node;
-    // if (!map_node->use_external_tf()) {
-    //         ov_tf_node = std::make_shared<VoxlTfNode>();
-    //         smip_exec.add_node(ov_tf_node);
-    // }
+    if (!map_node->use_external_tf()) {
+            ov_tf_node = std::make_shared<VoxlTfNode>();
+            smip_exec.add_node(ov_tf_node);
+    }
         
     // smip_exec.add_node(planner_node);
     smip_exec.spin();
